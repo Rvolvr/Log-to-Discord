8ca4e305dee83c2168edd07706e9d8aba179a7c9
Aaron F <10343946+Rvolvr@users.noreply.github.com>
2021-11-18 10:27:13 -0700

    Update log-to-discord.sh

--- log-to-discord.sh
+++ log-to-discord.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Tail will look at the last line(s) in the file established as variable 1, then with the -n switch set to 0 meaning that no previous lines will appear. The -F switch means that it will continuously read the file for changes. 
-# Piping that information into a function which will process each line to do what is in the parentheses. 
-# The parentheses means that it will send the LINE variable to see if it matches variable 3. 
-# After the completion of a grep, that information is sent as an HTML POST protocal, in JSON format.
-# Lastly we create the JSON package (with some line modifications) to the URL at variable 2.
-
 # Use: ./log-to-discord.sh <LogFilePath> <DiscordWebHook> <LogKeyword>
 
 tail -n0 -F "$1" | while read LINE; do
